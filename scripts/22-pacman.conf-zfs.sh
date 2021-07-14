cb09b6d10eba1ce8df487eb8762b39a975cbbfdb
Andrew Turner <andrewjamesturner0@gmail.com>
2021-07-14 13:49:31 +0100

    Add pacman.conf options

--- scripts/22-pacman.conf-zfs.sh
+++ scripts/22-pacman.conf-zfs.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 _pacman_conf='/mnt/etc/pacman.conf'
 
+sed -i '/^#Color/ c Color' "${_pacman_conf}"
+sed -i '/^#ParallelDownloads = 5/ c ParallelDownloads = 5' "${_pacman_conf}"
+
 if ! grep ILoveCandy "${_pacman_conf}"; then
     sed -i '/^\[options\]/a ILoveCandy' "${_pacman_conf}"
 fi
